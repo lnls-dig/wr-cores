f402d34252c27b43cc0fa931716341b2dd09be8d
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2014-07-17 14:39:50 +0200

    wr_softpll_ng: initial version of DMTD-based external reference PLL

--- syn/spec_1_1/wr_core_demo/Manifest.py
+++ syn/spec_1_1/wr_core_demo/Manifest.py
@@ -3,7 +3,7 @@ action = "synthesis"
 
 fetchto = "../../../ip_cores"
 
-top_module = "spec_top"
+#top_module = "spec_top"
 syn_device = "xc6slx45t"
 syn_grade = "-3"
 syn_package = "fgg484"
@@ -12,8 +12,8 @@ syn_project = "spec_top_wrc.xise"
 
 modules = { "local" : 
 						[ "../../../top/spec_1_1/wr_core_demo", 
-							"../../../platform",
-							"../../../ip_cores/general-cores",
-							"../../../ip_cores/etherbone-core",
-							"../../../ip_cores/gn4124-core"] 
-					}
+						  "../../../platform" ],
+	    "git" : 
+						[ "git://ohwr.org/hdl-core-lib/general-cores.git",
+						  "git://ohwr.org/hdl-core-lib/gn4124-core.git",
+						  "git://ohwr.org/hdl-core-lib/etherbone-core.git" ] };
--- testbench/top_level/Manifest.py
+++ testbench/top_level/Manifest.py
@@ -5,5 +5,5 @@ vlog_opt="+incdir+../../sim +incdir+gn4124_bfm"
 
 files = [ "main.sv" ]
 
-modules = { "local" :  [ "../..", "../../top/spec_1_1/wr_core_demo", "gn4124_bfm"] }
+modules = { "local" :  [ "../..", "../../top/spec_1_1/wr_core_demo", "../../../general-cores", "../../../gn4124-core", "../../../etherbone-core", "gn4124_bfm"] }
 
