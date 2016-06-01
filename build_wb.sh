5d74d3a63f8a8631111068ebccde5b67f0f3026c
Maciej Lipinski <maciej.lipinski@cern.ch>
2016-06-01 02:22:04 +0200

    fix wbgen generation for system verilog header

--- build_wb.sh
+++ build_wb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wbgen2  -D ./wb_doc.html -p wr_transmission_wbgen2_pkg.vhd -H record -V wr_transmission_wb.vhd --cstyle defines --lang vhdl -K ../../testbench/include/regs/wr_transmission_wb.vh wr_transmission_wb.wb
+wbgen2  -D ./wb_doc.html -p wr_transmission_wbgen2_pkg.vhd -H record -V wr_transmission_wb.vhd --cstyle defines --lang vhdl -K ../../testbench/include/regs/wr_transmission_wb.svh wr_transmission_wb.wb
