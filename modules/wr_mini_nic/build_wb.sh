cc3a501c1d95560ddbab3b46b5f4762eef260601
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2016-11-03 10:26:03 +0100

    minic: generate also *.h with wbgen

--- modules/wr_mini_nic/build_wb.sh
+++ modules/wr_mini_nic/build_wb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p doc
-wbgen2 -D ./doc/minic.html -V minic_wb_slave.vhd -p minic_wbgen2_pkg.vhd --cstyle defines --lang vhdl  -H record -K ../../sim/minic_regs.vh mini_nic.wb
+wbgen2 -D ./doc/minic.html -C minic_regs.h -V minic_wb_slave.vhd -p minic_wbgen2_pkg.vhd --cstyle defines --lang vhdl  -H record -K ../../sim/minic_regs.vh mini_nic.wb
