436abe6d174dc0d1c08d1a81dc1da4cf5c66b670
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2016-11-17 17:31:37 +0100

    testbench: trivial fix of paths in the Manifest

--- testbench/top_level/Manifest.py
+++ testbench/top_level/Manifest.py
@@ -5,5 +5,10 @@ vlog_opt="+incdir+../../sim +incdir+gn4124_bfm"
 
 files = [ "main.sv" ]
 
-modules = { "local" :  [ "../..", "../../top/spec_1_1/wr_core_demo", "../../../general-cores", "../../../gn4124-core", "../../../etherbone-core", "gn4124_bfm"] }
+modules = { "local" :  [ "../..",
+    "../../top/spec_1_1/wr_core_demo",
+    "../../ip_cores/general-cores",
+    "../../ip_cores/gn4124-core",
+    "../../ip_cores/etherbone-core",
+    "gn4124_bfm"] }
 
