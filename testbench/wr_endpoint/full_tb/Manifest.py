10ccc8c1906d320a0e82c618acfe97100f3e8521
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2017-01-20 11:10:15 +0100

    update testbeches Manifests to use new hdlmake

--- testbench/wr_endpoint/full_tb/Manifest.py
+++ testbench/wr_endpoint/full_tb/Manifest.py
@@ -1,9 +1,16 @@
 target = "xilinx"
 action = "simulation"
+syn_device = "xc6slx45t"
+syn_grade = "-3"
+syn_package = "fgg484"
+sim_tool = "modelsim"
+top_module = "main"
 files = "main.sv"
 fetchto = "../../../ip_cores"
 
 vlog_opt="+incdir+../../../sim +incdir+../../../sim/fabric_emu"
+
+include_dirs = [ "../../../sim" ]
 
 modules ={"git" : [    "git@ohwr.org:hdl-core-lib/general-cores.git" ],
 					"local" : ["../../../modules/wr_endpoint", 
