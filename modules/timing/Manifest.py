785b64c10dcbb5a45ac63b6c555fb12eb1b6d44d
Lucas Russo <lerwys@gmail.com>
2017-08-03 11:53:06 -0300

    timing: expose DMTD signals with dmtd_phase_meas_full component

--- modules/timing/Manifest.py
+++ modules/timing/Manifest.py
@@ -1,7 +1,8 @@
 files = ["dmtd_phase_meas.vhd",
+         "dmtd_phase_meas_full.vhd",
          "dmtd_with_deglitcher.vhd",
          "multi_dmtd_with_deglitcher.vhd",
          "hpll_period_detect.vhd",
          "pulse_gen.vhd",
          "pulse_stamper.vhd" ]
-         
+
