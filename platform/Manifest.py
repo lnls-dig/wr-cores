42f57420901dbe75887a3430c824bca1f6891d85
Tristan Gingold <tristan.gingold@cern.ch>
2024-07-16 16:46:51 +0200

    platform/Manifest.py: skip if no target defined
    Allow support of generic platforms

--- platform/Manifest.py
+++ platform/Manifest.py
@@ -1,4 +1,8 @@
-if target=="altera":
+import logging
+
+if 'target' not in globals():
+        logging.info("'target' is not defined, no platform selected")
+elif target=="altera":
 	modules = {"local" : "altera"}
 elif target=="xilinx":
 	modules = {"local" : "xilinx"}
